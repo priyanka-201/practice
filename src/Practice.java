75405b832400df7119b24122808f1b4d6b61157d
priyanka-201 <kp.priyanka03@gmail.com>
2023-08-15 18:58:38 +0530

    Practice

---
+++ src/Practice.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args)
+    {
+// to get input from the user //
+       Scanner input = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name=input.next();
+        System.out.println("name : "+name);
+    //to multiply 2 floating point number
+        Float a=23.34f;
+        Float b=45.67f;
+        double c=a*b;
+        System.out.println("Multiply of 2 number " +c);
+        // check even or odd
+
+    }
+}
