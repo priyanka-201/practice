820be97c81a2047ab4d56f520217d0d0ce6bf277
priyanka-201 <kp.priyanka03@gmail.com>
2023-08-15 19:37:20 +0530

    Practice1

---
+++ src/Practice1.java
@@ -0,0 +1,22 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        int number;
+        number=10;
+        System.out.println(number);
+        float b=3.4f;
+        System.out.println(b);
+        double c =32.9999;
+        System.out.println(c);
+        char v='a';
+        System.out.println(v);
+        int age=14;
+        boolean isMajor;
+if (age>18){
+    isMajor=true;
+}else {
+    isMajor=false;
+}
+
+        System.out.println("Major :"+isMajor);
+    }
+}
