1dbfe3fc72de7c384d8facf47b9942fecc098956
priyanka-201 <kp.priyanka03@gmail.com>
2023-08-16 11:37:41 +0530

    Variables & datatypes

---
+++ src/VariableDemo.java
@@ -0,0 +1,25 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        int number;
+        number=10;
+        number=30;
+        System.out.println(number);
+        float cgpa=7.5f;
+        System.out.println(cgpa);
+        double weight =75.78;
+        System.out.println("weight :"+weight);
+        char vowel;
+        vowel = 'a';
+        vowel = 'e';
+        System.out.println(vowel);
+        int age=15;
+        boolean isMajor;
+        if(age>18) {
+            isMajor=true;
+        } else {
+            isMajor=false;
+        }
+
+        System.out.println("Major :"+isMajor);
+    }
+}
